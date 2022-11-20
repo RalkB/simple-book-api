f0ca26551a01ac1513c30c398f55e0b6257f8c3a
guilherme Cordeiro de Almeida <guilhermeca002@gmail.com>
2022-11-20 20:43:01 -0300

    adding cors

--- server.js
+++ server.js
@@ -3,13 +3,20 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
 const BookCategoryRoutes = require('./routes/book-category');
 const BookRoutes = require('./routes/book');
 const categoryRoutes = require('./routes/category');
 const userRoutes = require('./routes/user');
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 
 app.use(BookCategoryRoutes);
 app.use(BookRoutes);
